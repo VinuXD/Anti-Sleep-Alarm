798106a09e77fb512218587e3520e2b896e3614a
VINU <vinuvarsath3@gmail.com>
2022-08-18 21:26:33 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,14 +5,11 @@ const int buzzer = 5;
 void setup() {
   pinMode(button, INPUT); // Button as Input
   pinMode(buzzer, OUTPUT); // Buzzer as Output
-  Serial.begin(9600); // Setting up Serial Port
 }
 
 void loop() {
 restart: // restart: flag to start the program again
-  Serial.println("WAITING");
   for (int i = 1; i <= 1000; i++) { // To check if the user presses the button in 10 seconds delay
-    Serial.println(i);
     delay(10); // for loop's 1000 and delay's 10 => 1000 * 10 = 10 Seconds
     if (digitalRead(button) == 1) // checking the button's value
       goto restart; // Going back to restart the countdown
@@ -22,7 +19,6 @@ restart: // restart: flag to start the program again
 
 // toneBuzzer() function will sound buzzer for 2 seconds
 void toneBuzzer() {
-  Serial.println("ALARM RINGING");
   digitalWrite(buzzer, HIGH);
   delay(2000);
   digitalWrite(buzzer, LOW);
